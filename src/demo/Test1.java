ced832bfbdb2010e08956dde5d4cc24bc0ac27d7
KRISHNA MOHAN <krishnamohan816@gmail.com>
2025-06-16 19:57:22 +0530

    First commit....

---
+++ src/demo/Test1.java
@@ -0,0 +1,19 @@
+package demo;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		
+		String hungry="yes";
+		
+		if(hungry == "yes") 
+		{
+			System.out.println("Eat Something");
+		}
+		else{
+			System.out.println("Do Work....");
+		}
+
+	}
+
+}
